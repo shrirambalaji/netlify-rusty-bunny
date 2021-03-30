058e2cee66ba52fa870d87156da0a454d9e61301
Shriram Balaji <shrirambalaji1996@gmail.com>
2021-03-31 01:29:03 +0530

    Check queryString params

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,18 @@ use netlify_lambda_http::{
 
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
+fn search(cmd: String) -> String {
+    todo!()
+}
+
 #[lambda(http)]
 #[tokio::main]
 async fn main(_: Request, _: Context) -> Result<impl IntoResponse, Error> {
-    Ok("🦀 Hello, Netlify 🦀")
+    Ok(format!(
+        "🦀 hello {}",
+        request
+            .query_string_parameters()
+            .get("cmd")
+            .unwrap_or_else(|| "from rusty-bunny! 🦀 🧡 🐰")
+    ))
 }
